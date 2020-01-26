5c8ada9ac430cd52299cc4b3b79bf3a7a0b8d3fd
keylesssky187 <60062880+keylesssky187@users.noreply.github.com>
2020-01-26 14:03:37 +0300

    punkt2_1

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("all ok");
+console.log("all ok")
+punkt2_1()
+
+function punkt2_1() {
+	//разница в сравнении
+	let d = {key: "value"};
+	let e = {key: "value"} == d; //не строгое сравение
+	let a = {key: "value"} === d; //строгое
+	console.log(a);
+	let b = d;
+	let c = b ===d;   //ссылаются на один и тот же объект
+	console.log(c);
+}
